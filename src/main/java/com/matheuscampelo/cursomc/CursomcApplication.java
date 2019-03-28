9d3f4d67ab128d6e11e6d7937cbd28fc9c7eea35
Matheus Campelo <matheuscampelocavalcante@hotmail.com>
2019-03-27 22:28:21 -0300

    instanciacao de objetos commandlinerunner

--- src/main/java/com/matheuscampelo/cursomc/CursomcApplication.java
+++ src/main/java/com/matheuscampelo/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.matheuscampelo.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.matheuscampelo.cursomc.model.Categoria;
+import com.matheuscampelo.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	@Autowired
+private CategoriaRepository repository;	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	Categoria cat1 = new Categoria (null, "Informática");
+	Categoria cat2 = new Categoria (null, "Escritório");
+	repository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
