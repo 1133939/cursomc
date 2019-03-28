bd9c7714e6c4a3cb99e2beb480aa25eb2ee15290
Matheus Campelo <matheuscampelocavalcante@hotmail.com>
2019-03-27 21:07:34 -0300

    testando modelo classe categoria

--- src/main/java/com/matheuscampelo/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/matheuscampelo/cursomc/resources/CategoriaResources.java
@@ -1,15 +1,25 @@
 package com.matheuscampelo.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.matheuscampelo.cursomc.model.Categoria;
 
 @RestController
 @RequestMapping(value="categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "REST";
+	public List<Categoria> list() {
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		List<Categoria> lista = Arrays.asList(cat1,cat2);
+		
+		return lista;
 	}
 }
