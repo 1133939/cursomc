41b766c4f9b21bf9b97cfc8f226be17aa7e1472d
Matheus Campelo <matheuscampelocavalcante@hotmail.com>
2019-03-27 22:15:30 -0300

    Criando repositorio e service categoria

---
+++ src/main/java/com/matheuscampelo/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.matheuscampelo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheuscampelo.cursomc.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
--- src/main/java/com/matheuscampelo/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/matheuscampelo/cursomc/resources/CategoriaResources.java
@@ -1,24 +1,26 @@
 package com.matheuscampelo.cursomc.resources;
 
-import java.util.Arrays;
-import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheuscampelo.cursomc.model.Categoria;
+import com.matheuscampelo.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="categorias")
 public class CategoriaResources {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> list() {
-		Categoria cat1 = new Categoria(1,"Informática");
-		Categoria cat2 = new Categoria(2,"Escritório");
-		List<Categoria> lista = Arrays.asList(cat1,cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria categoria = service.buscar(id);		
+		return ResponseEntity.ok().body(categoria);
 	}
 }
