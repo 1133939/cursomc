c76edf540699e14c336cb6ce3a2b1b95d8f884e8
Matheus Campelo <matheuscampelocavalcante@hotmail.com>
2019-04-01 20:00:12 -0300

    Deletando categoria sem filhos

--- src/main/java/com/matheuscampelo/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/matheuscampelo/cursomc/resources/CategoriaResources.java
@@ -40,4 +40,9 @@ public class CategoriaResources {
 		categoria = service.update(categoria);
 		return ResponseEntity.noContent().build();
 	}
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/matheuscampelo/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheuscampelo/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.matheuscampelo.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.matheuscampelo.cursomc.model.Categoria;
 import com.matheuscampelo.cursomc.repositories.CategoriaRepository;
+import com.matheuscampelo.cursomc.services.exceptions.DataIntegrityException;
 import com.matheuscampelo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,5 +30,13 @@ public class CategoriaService {
 	public Categoria update(Categoria categoria) {
 		find(categoria.getId());
 		return repository.save(categoria);
+	}
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repository.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir Categoria que possui Produtos");
+		}
 	}
 }
