75f8baa3c48efe430d1f556d001425ba01352127
Matheus Campelo <matheuscampelocavalcante@hotmail.com>
2019-04-04 20:25:44 -0300

    validacao email ja cadastrado

--- src/main/java/com/matheuscampelo/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/matheuscampelo/cursomc/repositories/ClienteRepository.java
@@ -1,10 +1,14 @@
 package com.matheuscampelo.cursomc.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.matheuscampelo.cursomc.model.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/matheuscampelo/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/matheuscampelo/cursomc/services/validation/ClienteInsertValidator.java
@@ -5,15 +5,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.matheuscampelo.cursomc.dto.ClienteNewDTO;
+import com.matheuscampelo.cursomc.model.Cliente;
 import com.matheuscampelo.cursomc.model.enums.TipoCliente;
+import com.matheuscampelo.cursomc.repositories.ClienteRepository;
 import com.matheuscampelo.cursomc.resources.exceptions.FieldMessage;
 import com.matheuscampelo.cursomc.services.validation.utils.BR;
 
 
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
-
+	@Autowired
+	private ClienteRepository repoCliente;
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,8 +32,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
 
-		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(((ClienteNewDTO) objDto).getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		Cliente aux = repoCliente.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
 		}
 
 		for (FieldMessage e : list) {
